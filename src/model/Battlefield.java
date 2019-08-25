2a3a1c9a5c2712dc12c8a05e0dace8fc412ad521
Cristian Lasso <crisca_25@hotmail.com>
2019-08-25 13:05:17 -0500

    Battlefield atributtes

--- src/model/Battlefield.java
+++ src/model/Battlefield.java
@@ -1,9 +1,47 @@
 package model;
 
 public class Battlefield {
-
-	public Battlefield() {
-		
+	
+	private int rows;
+	private int columns;
+	private int[][] battlefield;
+	
+	
+	public Battlefield(int rows, int columns) {
+		this.rows = rows;
+		this.columns = columns;
+		battlefield = new int[rows][columns];
 	}
+	
+	public int getRows() {
+		return rows;
+	}
+
+
+	public void setRows(int rows) {
+		this.rows = rows;
+	}
+
+
+	public int getColumns() {
+		return columns;
+	}
+
+
+	public void setColumns(int columns) {
+		this.columns = columns;
+	}
+
+
+	public int[][] getBattlefield() {
+		return battlefield;
+	}
+
+
+	public void setBattlefield(int[][] battlefield) {
+		this.battlefield = battlefield;
+	}
+
+
 	
 }
